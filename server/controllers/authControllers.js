ec7c0bfbb7ad481a6eb2d664fcc59029feb044dc
SrijitaBaksi <baksisrijita@gmail.com>
2025-07-28 23:28:12 +0530

    fix refresh issue in deployment

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -3,9 +3,9 @@ import jwt from 'jsonwebtoken'
 import User from '../models/User.js'
 
 const tokenOptions = {
-  httpOnly: false,
-  secure: false,
-  sameSite: 'Lax',
+  httpOnly: true,
+  secure: true,
+  sameSite: 'None',
   maxAge: 10 * 24 * 60 * 60 * 1000, // 10 days
   path: '/',
 };
