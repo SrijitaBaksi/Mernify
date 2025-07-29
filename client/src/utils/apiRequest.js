767d9959b6f904e622bce49a24566f24ebe802cc
Ananta2545 <chatterjeeanata091@gmail.com>
2025-07-29 23:16:42 +0530

    Add apiRequest

--- client/src/utils/apiRequest.js
+++ client/src/utils/apiRequest.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
     // baseURL:"http://localhost:5000/api",
     baseURL:"https://mernify-xk7x.onrender.com/api",
-     withCredentials: true,               
+     withCredentials: true,
      headers: {
     'Content-Type': 'application/json',
   },
